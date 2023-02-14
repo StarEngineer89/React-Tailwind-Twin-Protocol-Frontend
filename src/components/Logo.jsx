d0b7243c664d463c3ecb7f9b3ab7b5a544784798
StarEngineer89 <mrg8406@gmail.com>
2023-02-14 23:30:09 +0900

    home page done

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Banner() {
+function Logo({ ...other }) {
   return (
     <Link to="/" className="block" aria-label="Cruip">
-      <img src="/images/logo.png" alt="logo" className="w-[40px]" />
+      <img src="/images/logo.png" alt="logo" className="w-[40px]" {...other} />
     </Link>
   );
 }
 
-export default Banner;
+export default Logo;
